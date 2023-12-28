907133c6fe424bd5e1f6c920b610ca8466ecd56b
arnab <arnabchatterjee.ac.2@gmail.com>
2023-12-28 18:25:54 +0530

    feat -- added platform changing

--- main.js
+++ main.js
@@ -27,26 +27,28 @@ detailsTab.addEventListener("click", () => {
 
 const platforms = ["github", "youtube", "linkedin", "twitter"];
 const platformLinksMap = {};
-platforms.map((p) => {
-  platformLinksMap[p] = "";
-});
+
+let totalLinksAdded = 0
 
 const addLinkButton = document.querySelector(".add-link-btn");
 addLinkButton.addEventListener("click", (e) => {
   const linksEditor = document.querySelector(".all-links");
   const component = LinkComponent();
   linksEditor.innerHTML += component;
+  const selector = document.getElementById(String(totalLinksAdded)).querySelector("select")
+  selector.onchange = changePlatform
+
 });
 
 function LinkComponent() {
-  return `<div class="links-viewer">
+  return `<div class="links-viewer" id=${generateID()}>
             <div class="links-viewer-heading">
               <p>Link</p>
               <button>Remove</button>
             </div>
             <div class="links-viewer-links">
               <label for="platform">Platform</label>
-              <select name="platform" id="platform">
+              <select name="platform" id="platform" onchange="changePlatform()">
                 ${platforms.map(
                   (platformName, index) =>
                     `<option value=${index}>${platformName}</option>`
@@ -57,4 +59,14 @@ function LinkComponent() {
               <input type="text" name="link" id="link"/>
             </div>
           </div>`;
+}
+
+function changePlatform(e){
+  const platformValue = platforms[e.target.value] 
+  console.log(platformValue)
+}
+
+function generateID(){
+  totalLinksAdded+=1;
+  return totalLinksAdded
 }
